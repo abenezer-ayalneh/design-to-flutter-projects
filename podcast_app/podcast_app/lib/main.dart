caf29579ee52c614dc9eb8edce86e0cdc02bc7a5
abene42 <abene42@gmail.com>
2021-04-03 10:12:11 +0300

    partially commiting

--- podcast_app/podcast_app/lib/main.dart
+++ podcast_app/podcast_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:podcast_app/presenation/pages/login_page.dart';
+import 'package:podcast_app/presenation/pages/main_page.dart';
 import 'package:podcast_app/presenation/pages/onboarding_page.dart';
 import 'package:podcast_app/states/login_page_state.dart';
 import 'package:podcast_app/states/onboarding_page_state.dart';
@@ -30,21 +31,22 @@ class MyApp extends StatelessWidget {
           ],
           builder: (context) {
             return StateBuilder(
-                observeMany: [
-                  () => login_page_state,
-                  () => signup_page_state,
-                  () => onboarding_page_state,
-                ],
-                builder: (context, _) {
-                  return MaterialApp(
-                    debugShowCheckedModeBanner: false,
-                    title: 'Flutter Demo',
-                    theme: ThemeData(
-                      visualDensity: VisualDensity.adaptivePlatformDensity,
-                    ),
-                    home: OnboardingPage(),
-                  );
-                });
+              observeMany: [
+                () => login_page_state,
+                () => signup_page_state,
+                () => onboarding_page_state,
+              ],
+              builder: (context, _) {
+                return MaterialApp(
+                  debugShowCheckedModeBanner: false,
+                  title: 'Flutter Demo',
+                  theme: ThemeData(
+                    visualDensity: VisualDensity.adaptivePlatformDensity,
+                  ),
+                  home: LoginPage(),
+                );
+              },
+            );
           });
     });
   }
