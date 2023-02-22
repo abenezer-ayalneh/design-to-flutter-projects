d8a562d537df3e529108971c86f0e38174b9627c
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2023-02-22 05:51:16 +0300

    feat: add null safety to uvento and podcast apps

--- podcast_app_new/lib/main.dart
+++ podcast_app_new/lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:podcast_app_new/presenation/pages/login_page.dart';
-import 'package:podcast_app_new/presenation/pages/main_page.dart';
-import 'package:podcast_app_new/presenation/pages/onboarding_page.dart';
 import 'package:podcast_app_new/states/login_page_state.dart';
 import 'package:podcast_app_new/states/onboarding_page_state.dart';
 import 'package:podcast_app_new/states/signup_page_state.dart';
@@ -11,9 +9,9 @@ import 'package:states_rebuilder/states_rebuilder.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(
-    SystemUiOverlayStyle(
+    const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
-      statusBarIconBrightness: Brightness.light,
+      statusBarIconBrightness: Brightness.dark,
     ),
   );
   runApp(MyApp());
@@ -22,32 +20,34 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(builder: (context, child) {
-      return Injector(
-          inject: [
-            Inject(() => LoginPageState()),
-            Inject(() => SignupPageState()),
-            Inject(() => OnboardingPageState()),
-          ],
-          builder: (context) {
-            return StateBuilder(
-              observeMany: [
-                    () => login_page_state,
-                    () => signup_page_state,
-                    () => onboarding_page_state,
-              ],
-              builder: (context, _) {
-                return MaterialApp(
-                  debugShowCheckedModeBanner: false,
-                  title: 'Flutter Demo',
-                  theme: ThemeData(
-                    visualDensity: VisualDensity.adaptivePlatformDensity,
-                  ),
-                  home: LoginPage(),
-                );
-              },
-            );
-          });
-    },);
+    return ScreenUtilInit(
+      builder: (context, child) {
+        return Injector(
+            inject: [
+              Inject(() => LoginPageState()),
+              Inject(() => SignupPageState()),
+              Inject(() => OnboardingPageState()),
+            ],
+            builder: (context) {
+              return StateBuilder(
+                observeMany: [
+                  () => login_page_state,
+                  () => signup_page_state,
+                  () => onboarding_page_state,
+                ],
+                builder: (context, _) {
+                  return MaterialApp(
+                    debugShowCheckedModeBanner: false,
+                    title: 'Flutter Demo',
+                    theme: ThemeData(
+                      visualDensity: VisualDensity.adaptivePlatformDensity,
+                    ),
+                    home: LoginPage(),
+                  );
+                },
+              );
+            });
+      },
+    );
   }
 }
