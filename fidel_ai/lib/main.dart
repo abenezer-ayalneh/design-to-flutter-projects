d87ec8ae1c1f9268dd379767e65b52909af87e95
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2023-04-14 21:58:02 +0300

    feat: education or game onboarding page

--- fidel_ai/lib/main.dart
+++ fidel_ai/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:fidel_ai/pages/onboarding/education-or-game-onboarding.page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,25 +11,65 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: const ColorScheme(
-          brightness: Brightness.dark,
-          primary: Color(0xffF8B936),
-          onPrimary: Color(0xff000000),
-          secondary: Color(0xff008867),
-          onSecondary: Color(0xffffffff),
-          error: Color.fromARGB(255, 44, 4, 7),
-          onError: Color(0xffFB1A2D),
-          background: Color(0xff0D0D0D),
-          onBackground: Color(0xffffffff),
-          surface: Color(0xff2E2E2E),
-          onSurface: Color(0xffffffff),
-        ),
-        scaffoldBackgroundColor: const Color(0xff0D0D0D),
-      ),
-      home: const EducationOrGameOnboardingPage(),
+    return ScreenUtilInit(
+      designSize: const Size(428, 919),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            fontFamily: 'Poppins',
+            textTheme: TextTheme(
+              titleLarge: TextStyle(
+                  fontSize: 65.sp,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white),
+              titleMedium: TextStyle(
+                  fontSize: 28.sp,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white),
+              titleSmall: TextStyle(
+                fontSize: 25.sp,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              ),
+              bodyLarge:  TextStyle(
+                fontSize: 18.sp,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              ),
+              bodyMedium:  TextStyle(
+                fontSize: 15.sp,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              ),
+              bodySmall:  TextStyle(
+                fontSize: 12.sp,
+                fontWeight: FontWeight.w100,
+                color: Colors.white,
+              ),
+
+            ),
+            colorScheme: const ColorScheme(
+              brightness: Brightness.dark,
+              primary: Color(0xffF8B936),
+              onPrimary: Color(0xff000000),
+              secondary: Color(0xff008867),
+              onSecondary: Color(0xffffffff),
+              error: Color.fromARGB(255, 44, 4, 7),
+              onError: Color(0xffFB1A2D),
+              background: Color(0xff0D0D0D),
+              onBackground: Color(0xffffffff),
+              surface: Color(0xff2E2E2E),
+              onSurface: Color(0xffffffff),
+            ),
+            scaffoldBackgroundColor: const Color(0xff0D0D0D),
+          ),
+          home: child,
+        );
+      },
+      child: const EducationOrGameOnboardingPage(),
     );
   }
 }
--- fidel_ai/lib/pages/onboarding/education-or-game-onboarding.page.dart
+++ fidel_ai/lib/pages/onboarding/education-or-game-onboarding.page.dart
@@ -1,15 +1,128 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class EducationOrGameOnboardingPage extends StatelessWidget {
   const EducationOrGameOnboardingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
-        child: Center(child: Text('Education or Game')),
+        child: Stack(
+          children: [
+            Center(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30.sp),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        width: 428.sp,
+                        height: 379.sp,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(
+                                'assets/images/kid_picture_heart_hand.png'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 52.sp,
+                      ),
+                      SizedBox(
+                        width: 194.sp,
+                        child: Text(
+                          'What do\nyou Like to do',
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleMedium
+                              ?.copyWith(height: 1),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15.sp,
+                      ),
+                      SizedBox(
+                        width: 340.sp,
+                        child: Text(
+                          'The platforms that you are going to use for adobe premier pro.',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodySmall
+                              ?.copyWith(fontSize: 15.sp),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 35.sp,
+                      ),
+                      Container(
+                        height: 52.sp,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15.sp),
+                        ),
+                        child: InkWell(
+                          onTap: () => print('object'),
+                          borderRadius: BorderRadius.circular(15.sp),
+                          child: Ink(
+                            decoration: BoxDecoration(
+                              color: Theme.of(context).colorScheme.primary,
+                              borderRadius: BorderRadius.circular(15.sp),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 14.sp,
+                      ),
+                      FilledButton(
+                        onPressed: () => print('button'),
+                        child: Text('Education'),
+                        style: ButtonStyle(),
+                      ),
+                      // Container(
+                      //   height: 52.sp,
+                      //   width: double.infinity,
+                      //   decoration: BoxDecoration(
+                      //     borderRadius: BorderRadius.circular(15.sp),
+                      //   ),
+                      //   child: InkWell(
+                      //     onTap: () => print('object'),
+                      //     borderRadius: BorderRadius.circular(15.sp),
+                      //     child: Ink(
+                      //       decoration: BoxDecoration(
+                      //         color: Theme.of(context).colorScheme.secondary,
+                      //         borderRadius: BorderRadius.circular(15.sp),
+                      //       ),
+                      //     ),
+                      //   ),
+                      // ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                bottom: 17.sp,
+              ),
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Text(
+                  'Powered by Ablaze Labs',
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodySmall
+                      ?.copyWith(fontWeight: FontWeight.w300),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
