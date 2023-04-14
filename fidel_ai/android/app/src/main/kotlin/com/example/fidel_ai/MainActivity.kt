348faf8e6710a20a22107bd21cab654e99d2ce12
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2023-04-14 18:11:08 +0300

    chore: add fidel ai

---
+++ fidel_ai/android/app/src/main/kotlin/com/example/fidel_ai/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fidel_ai
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ fidel_ai/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:fidel_ai/pages/home.page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: const ColorScheme(
+          brightness: Brightness.dark,
+          primary: Color(0xffF8B936),
+          onPrimary: Color(0xff000000),
+          secondary: Color(0xff008867),
+          onSecondary: Color(0xffffffff),
+          error: Color.fromARGB(255, 44, 4, 7),
+          onError: Color(0xffFB1A2D),
+          background: Color(0xff0D0D0D),
+          onBackground: Color(0xffffffff),
+          surface: Color(0xff2E2E2E),
+          onSurface: Color(0xffffffff),
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
